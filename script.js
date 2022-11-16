a4c9888cf27345ff28a160ab5560c8280afb70f0
rabianur akdaş <yamansena6@gmail.com>
2022-11-16 04:55:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("menu").style.height = "100%";
+    document.getElementById("close-icon").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu").style.height = "0%";
+    document.getElementById("close-icon").style.display = "none";
+}
